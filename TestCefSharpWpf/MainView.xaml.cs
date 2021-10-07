72171ce6dd52b5ded3457e0b67ed51e30dab505a
kpreisser <kpreisser@users.noreply.github.com>
2021-10-07 12:34:42 +0200

    Add a WPF app that can be used to reproduce the issue with D3DCompiler_47.dll on Windows ARM64.

---
+++ TestCefSharpWpf/MainView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+
+using CefSharp.Wpf.HwndHost;
+
+namespace TestCefSharpWpf
+{
+    public partial class MainView : Window
+    {
+        private readonly ChromiumWebBrowser browser;
+
+        public MainView()
+            : base()
+        {
+            this.InitializeComponent();
+
+            this.browser = new ChromiumWebBrowser();
+            this.browserGrid.Children.Add(this.browser);
+
+            string url = "https://www.google.com";
+            LoadBrowserUrl(url);
+        }
+
+        private void LoadBrowserUrl(string url)
+        {
+            // Need to check if the browser is already initialized.
+            // would not work in that case.
+            if (this.browser.IsBrowserInitialized)
+            {
+                this.browser.Load(url);
+            }
+            else
+            {
+                void HandleInitializedChanged(object s2, EventArgs e2)
+                {
+                    if (this.browser.IsBrowserInitialized)
+                    {
+                        this.browser.IsBrowserInitializedChanged -= HandleInitializedChanged;
+                        this.browser.Load(url);
+                    }
+                };
+
+                this.browser.IsBrowserInitializedChanged += HandleInitializedChanged;
+            }
+        }
+    }
+}
---
+++ TestCefSharpWpf/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+using CefSharp;
+using CefSharp.BrowserSubprocess;
+using CefSharp.Wpf.HwndHost;
+
+namespace TestCefSharpWpf
+{    
+    internal static class Program
+    {
+        [STAThread]
+        private static int Main(string[] args)
+        {
+            // Determine if we need to run the CEFSharp browser subprocess.
+            if (args.Length > 0 && args[0].StartsWith( "--type=", StringComparison.Ordinal)) {
+                return SelfHost.Main(args);
+            }
+            else {
+                // Run the regular application.
+                // Initialize CEFSharp.
+                InitializeCefSharp();
+
+                // Run the program with WPF GUI.
+                App.Main();
+
+                // Shutdown CEFSharp.
+                Cef.Shutdown();
+                return Environment.ExitCode;
+            }
+        }
+
+        private static void InitializeCefSharp()
+        {
+            CefSharpSettings.ShutdownOnExit = false;
+
+            var settings = new CefSettings {
+                LogSeverity = LogSeverity.Default,
+                LogFile = Path.GetFullPath(@"TestCefSharpChromiumLog.txt"),
+                CachePath = Path.GetFullPath(@"CEF-Cache"),
+            };
+
+            // Self-host the subprocess.
+            using (var currentProcess = Process.GetCurrentProcess())
+                settings.BrowserSubprocessPath = currentProcess.MainModule.FileName;
+
+            if (!Cef.Initialize(settings))
+                throw new InvalidOperationException("Could not initialize CefSharp.");
+
+            Cef.EnableHighDPISupport();
+        }
+    }
+}
